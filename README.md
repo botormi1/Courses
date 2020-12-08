b6abdaa95c933e1fbce4af4cf3f23310f3535cce
botormi1 <botormi1@gmail.com>
2020-12-08 21:19:57 +0100

    [Udemy.Languages.Java] Initial commit. || Adding S00_HelloJava section.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Courses
+
+This repository documents all the courses i have taken.
---
+++ Udemy/Languages/Java/S00_HelloJava/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, Java" on the console screen.
+        System.out.println("Hello, Java!");
+    }
+}
